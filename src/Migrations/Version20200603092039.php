00ed68d58ea64d803405fd91e2252f532ebc9219
Green <regnault.marien@gmail.com>
2020-06-03 16:03:01 +0200

    Implementation of graphic charter

---
+++ src/Migrations/Version20200603092039.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200603092039 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE departure (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE shelter (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE arrival (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, shelter_id INT DEFAULT NULL, mobicoop_id INT NOT NULL, status VARCHAR(100) NOT NULL, is_active TINYINT(1) NOT NULL, role VARCHAR(100) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_8D93D64954053EC0 (shelter_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE schedule_volunteer (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, is_morning TINYINT(1) NOT NULL, is_afternoon TINYINT(1) NOT NULL, date DATETIME NOT NULL, INDEX IDX_9E46A0AB9D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trip (id INT AUTO_INCREMENT NOT NULL, departure_id_id INT NOT NULL, arrival_id_id INT NOT NULL, date DATETIME NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_7656F53B8E71F04D (departure_id_id), INDEX IDX_7656F53B88C7139 (arrival_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trip_user (trip_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_A6AB4522A5BC2E0E (trip_id), INDEX IDX_A6AB4522A76ED395 (user_id), PRIMARY KEY(trip_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64954053EC0 FOREIGN KEY (shelter_id) REFERENCES shelter (id)');
+        $this->addSql('ALTER TABLE schedule_volunteer ADD CONSTRAINT FK_9E46A0AB9D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE trip ADD CONSTRAINT FK_7656F53B8E71F04D FOREIGN KEY (departure_id_id) REFERENCES departure (id)');
+        $this->addSql('ALTER TABLE trip ADD CONSTRAINT FK_7656F53B88C7139 FOREIGN KEY (arrival_id_id) REFERENCES arrival (id)');
+        $this->addSql('ALTER TABLE trip_user ADD CONSTRAINT FK_A6AB4522A5BC2E0E FOREIGN KEY (trip_id) REFERENCES trip (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE trip_user ADD CONSTRAINT FK_A6AB4522A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE trip DROP FOREIGN KEY FK_7656F53B8E71F04D');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D64954053EC0');
+        $this->addSql('ALTER TABLE trip DROP FOREIGN KEY FK_7656F53B88C7139');
+        $this->addSql('ALTER TABLE schedule_volunteer DROP FOREIGN KEY FK_9E46A0AB9D86650F');
+        $this->addSql('ALTER TABLE trip_user DROP FOREIGN KEY FK_A6AB4522A76ED395');
+        $this->addSql('ALTER TABLE trip_user DROP FOREIGN KEY FK_A6AB4522A5BC2E0E');
+        $this->addSql('DROP TABLE departure');
+        $this->addSql('DROP TABLE shelter');
+        $this->addSql('DROP TABLE arrival');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE schedule_volunteer');
+        $this->addSql('DROP TABLE trip');
+        $this->addSql('DROP TABLE trip_user');
+    }
+}
