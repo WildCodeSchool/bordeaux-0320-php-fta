928b9219ca2f99c2dfdd751606d77e991bae3813
Green <regnault.marien@gmail.com>
2020-06-12 14:18:33 +0200

    Fixed

--- src/Migrations/Version20200612121211.php
+++ src/Migrations/Version20200612121211.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200609143150 extends AbstractMigration
+final class Version20200612121211 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,9 +23,13 @@ final class Version20200609143150 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE departure (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE shelter (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE arrival (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, mobicoop_id INT NOT NULL, status VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE schedule_volunteer (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, is_morning TINYINT(1) NOT NULL, is_afternoon TINYINT(1) NOT NULL, date DATETIME NOT NULL, INDEX IDX_9E46A0ABA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE trip (id INT AUTO_INCREMENT NOT NULL, departure_id INT NOT NULL, arrival_id INT NOT NULL, date DATETIME NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_7656F53B7704ED06 (departure_id), INDEX IDX_7656F53B62789708 (arrival_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE trip_user (trip_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_A6AB4522A5BC2E0E (trip_id), INDEX IDX_A6AB4522A76ED395 (user_id), PRIMARY KEY(trip_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE schedule_volunteer ADD CONSTRAINT FK_9E46A0ABA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE trip ADD CONSTRAINT FK_7656F53B7704ED06 FOREIGN KEY (departure_id) REFERENCES departure (id)');
         $this->addSql('ALTER TABLE trip ADD CONSTRAINT FK_7656F53B62789708 FOREIGN KEY (arrival_id) REFERENCES arrival (id)');
         $this->addSql('ALTER TABLE trip_user ADD CONSTRAINT FK_A6AB4522A5BC2E0E FOREIGN KEY (trip_id) REFERENCES trip (id) ON DELETE CASCADE');
@@ -39,9 +43,14 @@ final class Version20200609143150 extends AbstractMigration
 
         $this->addSql('ALTER TABLE trip DROP FOREIGN KEY FK_7656F53B7704ED06');
         $this->addSql('ALTER TABLE trip DROP FOREIGN KEY FK_7656F53B62789708');
+        $this->addSql('ALTER TABLE schedule_volunteer DROP FOREIGN KEY FK_9E46A0ABA76ED395');
+        $this->addSql('ALTER TABLE trip_user DROP FOREIGN KEY FK_A6AB4522A76ED395');
         $this->addSql('ALTER TABLE trip_user DROP FOREIGN KEY FK_A6AB4522A5BC2E0E');
         $this->addSql('DROP TABLE departure');
+        $this->addSql('DROP TABLE shelter');
         $this->addSql('DROP TABLE arrival');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE schedule_volunteer');
         $this->addSql('DROP TABLE trip');
         $this->addSql('DROP TABLE trip_user');
     }
