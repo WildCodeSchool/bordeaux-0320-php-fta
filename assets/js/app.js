28ff45c38949f84a17dd4c36f5baebad8da9629f
Green <regnault.marien@gmail.com>
2020-06-21 17:17:07 +0200

    CSS refactored + small Twig improvements

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,8 @@ import './Materialize/init';
 import 'material-design-icons/iconfont/material-icons.css';
 import 'materialize-css/dist/js/materialize';
 import './schedule/calendar';
-import './schedule/clipboard';
 import './axios/ajaxSchedule';
 
 require('../scss/app.scss');
-
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
