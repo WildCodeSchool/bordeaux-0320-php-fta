045fc64f69e81a1d2583505a3e121f514eb63403
Green <regnault.marien@gmail.com>
2020-07-15 17:27:41 +0200

    CSS added to the accompagnement table in admin part + small changes in CSS in  admin part

--- assets/js/admin/paginatorTrips.js
+++ assets/js/admin/paginatorTrips.js
@@ -6,7 +6,7 @@ const paginators = document.getElementsByClassName('paginator');
 
 for (let i = 0; i < paginators.length; i++) {
     paginators[i].addEventListener('click', (evt) => {
-        const button = evt.target;
+        const button = paginators[i];
         const paginatorType = button.dataset.paginator;
         const limit = Number(button.dataset.limit);
         showPage(limit).then((value) => {
